e0598dc8c818ef31adc139867c1b80de2818ecd6
Alyssa Phillips <arphillips@ucdavis.edu>
2021-06-25 11:27:03 -0700

    add cpus-per-task specification

--- submit.sh
+++ submit.sh
@@ -4,5 +4,5 @@ snakemake --jobs 50 --use-conda \
 --rerun-incomplete \
 --latency-wait 120 \
 --cluster-config submit.json \
---cluster "sbatch --mem {cluster.mem} -J {cluster.name} --time {cluster.time} -p {cluster.p}"
+--cluster "sbatch --mem {cluster.mem} -J {cluster.name} --time {cluster.time} -p {cluster.p} -o {cluster.o} --cpus-per-task {cluster.cpus-per-task}"
 
